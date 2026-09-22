94d0865b56c7ee3a090825c804683e1091af6380
mdzabedhossain <mdzabedhossain@gmail.com>
2026-09-22 13:28:17 +0600

    Update NovaCart for Google Play Android 16 target

--- android-ecommerce/app/build.gradle.kts
+++ android-ecommerce/app/build.gradle.kts
@@ -2,6 +2,14 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
-android { namespace = "com.zabed.novacart"; compileSdk = 35
-    defaultConfig { applicationId = "com.zabed.novacart"; minSdk = 23; targetSdk = 35; versionCode = 1; versionName = "1.0" }
+android {
+    namespace = "com.zabed.novacart"
+    compileSdk = 36
+    defaultConfig {
+        applicationId = "com.zabed.novacart"
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
 }
